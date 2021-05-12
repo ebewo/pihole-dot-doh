951ded4a134a765e329b9450797b855ca830a68c
Nathan Beka <nathan.beka@gmail.com>
2021-05-12 10:18:30 +0100

    disable cloudflared
    stubby is verbose level 5

--- stuff/install.sh
+++ stuff/install.sh
@@ -7,13 +7,13 @@ mkdir -p /etc/services.d/pihole-dot-doh
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
 # Copy config file if not exists
 echo 'cp -n /temp/stubby.yml /config/' >> /etc/services.d/pihole-dot-doh/run
-echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
+# echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
 # run stubby in background
 echo 's6-echo "Starting stubby"' >> /etc/services.d/pihole-dot-doh/run
-echo 'stubby -g -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
+echo 'stubby -v 5 -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
 # run cloudflared in foreground
-echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
-echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
+# echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
+# echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
 
 # finish file
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/finish
