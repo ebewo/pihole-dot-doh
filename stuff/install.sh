d365ebb20e684b97f784923e772e3bf0bf82bf43
Nathan Beka <nathan.beka@gmail.com>
2021-05-12 09:50:32 +0100

    disable cloudflared

--- stuff/install.sh
+++ stuff/install.sh
@@ -7,17 +7,17 @@ mkdir -p /etc/services.d/pihole-dot-doh
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
 # Copy config file if not exists
 echo 'cp -n /temp/stubby.yml /config/' >> /etc/services.d/pihole-dot-doh/run
-echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
+# echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
 # run stubby in background
 echo 's6-echo "Starting stubby"' >> /etc/services.d/pihole-dot-doh/run
 echo 'stubby -g -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
 # run cloudflared in foreground
-echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
-echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
+# echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
+# echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
 
 # finish file
 echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/finish
 echo 's6-echo "Stopping stubby"' >> /etc/services.d/pihole-dot-doh/finish
 echo 'killall -9 stubby' >> /etc/services.d/pihole-dot-doh/finish
-echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/pihole-dot-doh/finish
-echo 'killall -9 cloudflared' >> /etc/services.d/pihole-dot-doh/finish
+# echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/pihole-dot-doh/finish
+# echo 'killall -9 cloudflared' >> /etc/services.d/pihole-dot-doh/finish
